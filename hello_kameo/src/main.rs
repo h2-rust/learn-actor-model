c5628f405b9118eb67a45368d0a88d913c2e9b13
Hyunghwan Shin <hwan.shin@gmail.com>
2026-02-03 01:27:46 +0000

    add comments

--- hello_kameo/src/main.rs
+++ hello_kameo/src/main.rs
@@ -11,14 +11,17 @@ impl Message<Greet> for HelloActor {
     type Reply = ();
 
     async fn handle(&mut self, msg: Greet, _ctx: &mut Context<Self, Self::Reply>) -> Self::Reply {
-        println!("Actor received: {}", msg.m);
+        println!("Message received: {}", msg.m);
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // 1. Spawn the actor: This starts the actor loop in the background
     let actor_ref = HelloActor::spawn(HelloActor);
 
+    // 2. Send the message
+    // We use .tell() to create the message envelope, and .send().await to deliver it
     actor_ref.tell(Greet { m: "Hello, world!".to_string() } ).send().await.unwrap();
     
 }
