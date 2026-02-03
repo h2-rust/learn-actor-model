986d6faf48da1114f9f54a50ee10bec075688acc
Hyunghwan Shin <hwan.shin@gmail.com>
2026-02-03 00:52:32 +0000

    Create an actor and send it a message

--- hello_kameo/src/main.rs
+++ hello_kameo/src/main.rs
@@ -1,7 +1,24 @@
+use kameo::prelude::*;
+
 struct Greet {
-    m: String
+    pub m: String
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Actor)]
+struct HelloActor;
+
+impl Message<Greet> for HelloActor {
+    type Reply = ();
+
+    async fn handle(&mut self, msg: Greet, _ctx: &mut Context<Self, Self::Reply>) -> Self::Reply {
+        println!("Actor received: {}", msg.m);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let actor_ref = HelloActor::spawn(HelloActor);
+
+    actor_ref.tell(Greet { m: "Hello, world!".to_string() } ).send().await.unwrap();
+    
 }
